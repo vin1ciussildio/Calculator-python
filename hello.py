2e368bfbdef3d16b1277f170ed76ad695637c0e5
vinicius sildio <144827101+vin1ciussildio@users.noreply.github.com>
2024-11-19 10:50:27 -0300

    Simple Python Calculator V2
    This is the second version of the python calculator , in this version i made the 4 basics operations with pre-defined values , only use "prints" and Variables , nothing too complicated for now....

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print ("hello world")
+Nome = "Hyde"
+num1 = 23 
+num2 = 32 
+soma = num1 + num2
+multiplicacao = num1*num2
+divisao = num1/num2
+subitracao = num1-num2
+
+print ("test")
+
+print (f"ola {Nome} seja muito bem vindo(a) a minha calculadora ^W^")
+print (f"O primeiro numero usado para os cauculos e:  {num1}")
+print (f"O segundo numero usado para os cauculos e:  {num2}")
+print (f"A soma dos dois numeros e:  {soma}")
+print (f"A multiplicacao dos dois numeros e::  {multiplicacao}")
+print (f"A divisao dos dois numeros e:  {divisao}")
+print (f"A subitracao dos dois numeros e:  {subitracao}")
